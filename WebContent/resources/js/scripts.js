81a792f70ba2b07db9a79c53a13de939fd22085a
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-06-05 13:30:43 -0300

    syntax(scripts): changes in scripts

--- WebContent/resources/js/scripts.js
+++ WebContent/resources/js/scripts.js
@@ -1,32 +1,31 @@
 $(() => {
-	const confirmDeleteItem = () => {
-		return Swal.fire({
-			  title: 'Você tem certeza?',
-			  text: "Você deseja realmente remover este registro?",
-			  type: 'warning',
-			  showCancelButton: true,
-			  confirmButtonColor: '#3085d6',
-			  cancelButtonColor: '#d33',
-			  confirmButtonText: 'Sim, remover!',
-			  cancelButtonText: 'Não, voltar.'
-			}).then(result => result.value ? true : false)
-	};
-	
-	$('.btnDelete').click(event=> {
-		if(!$(event.target).hasClass('confirmed')) {
-			event.preventDefault();
-			confirmDeleteItem().then(confirm => {
-				console.log(confirm)
-				if(confirm){
-					$(event.target).addClass('confirmed')
-					$(event.target).trigger("click");
-				}
-			});
-			
-			return false;
-		};
-		
-		return true;
-	});
-	
+  const confirmDeleteItem = () => {
+    return Swal.fire({
+      title: "Você tem certeza?",
+      text: "Você deseja realmente remover este registro?",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, remover!",
+      cancelButtonText: "Não, voltar."
+    }).then(result => (result.value ? true : false));
+  };
+
+  $(".btnDelete").click(event => {
+    if (!$(event.target).hasClass("confirmed")) {
+      event.preventDefault();
+      confirmDeleteItem().then(confirm => {
+        console.log(confirm);
+        if (confirm) {
+          $(event.target).addClass("confirmed");
+          $(event.target).trigger("click");
+        }
+      });
+
+      return false;
+    }
+
+    return true;
+  });
 });
