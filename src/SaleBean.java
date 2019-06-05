96e667b32ac54683628086a3b5f67abe90c3d741
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-06-05 13:32:27 -0300

    feat(sales): finished sales crud

---
+++ src/SaleBean.java
@@ -0,0 +1,163 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@ManagedBean(name = "saleBean", eager = true)
+public class SaleBean {
+	int id;
+	int customerId;
+	int vehicleId;
+
+	ArrayList<SaleBean> salesList;
+	Connection dbConnection;
+	PreparedStatement pstmt;
+	Map<String, Object> sessionData = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return the customerId
+	 */
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	/**
+	 * @return the vehicleId
+	 */
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	/**
+	 * @param vehicleId the vehicleId to set
+	 */
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public Connection getDatabase() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String dbUrl = "jdbc:mysql://localhost:3306/rentscar", dbUsername = "root", dbPassword = "";
+			this.dbConnection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return this.dbConnection;
+	}
+
+	public ArrayList<SaleBean> salesList() {
+		System.out.println("Execute salesList()");
+		try {
+			salesList = new ArrayList<SaleBean>();
+
+			Statement stmt = this.getDatabase().createStatement();
+			ResultSet resultObj = stmt.executeQuery("SELECT * FROM sales");
+
+			while (resultObj.next()) {
+				SaleBean sale = new SaleBean();
+				sale.setId(resultObj.getInt("id"));
+				sale.setCustomerId(resultObj.getInt("customer_id"));
+				sale.setVehicleId(resultObj.getInt("vehicle_id"));
+				salesList.add(sale);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return salesList;
+	}
+
+	public String save(SaleBean newSaleObj) {
+		System.out.println("Execute save()");
+		try {
+			pstmt = this.getDatabase().prepareStatement("INSERT INTO sales (customer_id, vehicle_id) VALUES (?, ?)");
+			pstmt.setInt(1, newSaleObj.getCustomerId());
+			pstmt.setInt(2, newSaleObj.getVehicleId());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return "/index.xhtml?faces-redirect=true";
+	}
+
+	public String delete(int id) {
+		System.out.println("Execute delete()");
+		try {
+			pstmt = this.getDatabase().prepareStatement("DELETE FROM sales WHERE id = " + id);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return "/index.xhtml?faces-redirect=true";
+	}
+
+	public String edit(int id) {
+		System.out.println("Execute edit()");
+		SaleBean sale = null;
+
+		try {
+			Statement stmt = this.getDatabase().createStatement();
+			ResultSet resultObj = stmt.executeQuery("SELECT * FROM sales WHERE id=" + (id));
+			resultObj.next();
+
+			sale = new SaleBean();
+			sale.setId(resultObj.getInt("id"));
+			sale.setId(resultObj.getInt("id"));
+			sale.setCustomerId(resultObj.getInt("customer_id"));
+			sale.setVehicleId(resultObj.getInt("vehicle_id"));
+			sessionData.put("saleToUpdate", sale);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return "/sales/edit.xhtml?faces-redirect=true";
+	}
+
+	public String update(SaleBean saleToUpdate) {
+		System.out.println("Execute update()");
+		try {
+			pstmt = this.getDatabase().prepareStatement("UPDATE sales SET customer_id=?, vehicle_id=? WHERE id=?");
+			pstmt.setInt(1, saleToUpdate.getCustomerId());
+			pstmt.setInt(2, saleToUpdate.getVehicleId());
+			pstmt.setInt(3, saleToUpdate.getId());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return "/index.xhtml?faces-redirect=true";
+	}
+}
